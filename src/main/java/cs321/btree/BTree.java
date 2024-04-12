46894dc651c3a6a70a35c4dac785a21b4d484f5b
Caleb Tolman <calebtolman@u.boisestate.edu>
2024-04-12 17:55:59 -0600

    Checkpoint 1

--- src/main/java/cs321/btree/BTree.java
+++ src/main/java/cs321/btree/BTree.java
@@ -7,8 +7,13 @@ import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
-public class BTree implements BTreeInterface
-{
+/**
+ * Represents a B-Tree structure that provides efficient data insertion, deletion,
+ * and lookup. This implementation specifically supports disk-based operations
+ * to accommodate large data sets that do not fit into main memory.
+ */
+public class BTree implements BTreeInterface {
+	
     static private final int DEGREE = 51; // Constant for this project
 
     private long size; // BTree size in Bytes
@@ -18,10 +23,24 @@ public class BTree implements BTreeInterface
     private BTreeNode root;
     private FileChannel channel;
 
+    /**
+     * Constructs a BTree using the default degree and initializes it from a file
+     * if it exists, or creates a new one if it does not.
+     *
+     * @param filePath the path to the file that stores the B-Tree on disk
+     */
     public BTree(String filePath) {
         this(filePath, DEGREE);
     }
 
+    /**
+     * Constructs a BTree from a specified file path and degree.
+     * If the file exists, it reads the BTree metadata and root from the file.
+     * Otherwise, it initializes a new empty BTree with a specified degree.
+     *
+     * @param filePath the path of the file to store the BTree
+     * @param degree   the minimum degree of the BTree
+     */
     public BTree(String filePath, int degree) {
         this.size = 0;
         this.height = 0;
@@ -67,6 +86,13 @@ public class BTree implements BTreeInterface
         }
     }
 
+    /**
+     * Reads the data of a BTreeNode from disk at a specified position.
+     * Converts the ByteBuffer to a byte array for further processing.
+     *
+     * @param nodePointer the pointer to the node's position in the file
+     * @return byte array containing the node's data
+     */
     public byte[] diskRead(long nodePointer) {
         try {
             // Calculate the position of the node in the file based on the nodePointer
@@ -88,113 +114,135 @@ public class BTree implements BTreeInterface
         return null;
     }
   
-  	public void diskWrite(BTreeNode node, FileChannel fileChannel) throws IOException {
+    /**
+     * Writes a BTreeNode's data to disk at the node's specified disk address.
+     * Ensures that the node data is written as a single atomic operation.
+     *
+     * @param node        the BTreeNode to write to disk
+     * @param fileChannel the FileChannel to use for writing
+     * @throws IOException if there is an error during writing
+     */
+    public void diskWrite(BTreeNode node, FileChannel fileChannel) throws IOException {
+        // Determine the size of the node to allocate a buffer of appropriate size
         ByteBuffer buffer = ByteBuffer.allocate(getNodeDiskSize(node));
 
-        // Start by writing the node metadata
+        // Write the number of keys in the node
         buffer.putInt(node.n);
+        // Write the leaf status as a byte (1 for true, 0 for false)
         buffer.put((byte) (node.isLeaf ? 1 : 0));
 
-        // Write keys
+        // Write all keys in this node
         for (int i = 0; i < node.n; i++) {
             buffer.putLong(node.keys[i]);
         }
 
-        // If not a leaf, write children addresses
+        // If the node is not a leaf, write addresses of its children
         if (!node.isLeaf) {
             for (int i = 0; i <= node.n; i++) {  // note that there are n+1 children
                 buffer.putLong(node.children[i]);
             }
         }
 
-        buffer.flip();  // prepare buffer for writing
-        fileChannel.position(node.diskAddress);  // Set position in file (if needed)
+        // Prepare the buffer to be written by setting the position back to the start
+        buffer.flip();
+        // Set the position in the file to the node's disk address
+        fileChannel.position(node.diskAddress);
+        // Write the buffer to the file at the current position
         fileChannel.write(buffer);
-        fileChannel.force(true);  // ensure changes are written to disk
+        // Optionally force writing to disk for data integrity
+        fileChannel.force(true);
+    }
+
+    /**
+     * Calculates the required disk size for a BTreeNode based on its properties.
+     *
+     * @param node the BTreeNode for which to calculate the disk size
+     * @return the size in bytes required to store the node on disk
+     */
+    private int getNodeDiskSize(BTreeNode node) {
+        int size = Integer.BYTES; // Space for 'n', the number of keys
+        size += 1; // Space for the leaf indicator (1 byte)
+        size += Long.BYTES * node.keys.length; // Space for keys
+
+        // If not a leaf, also allocate space for addresses of children
+        if (!node.isLeaf) {
+            size += Long.BYTES * node.children.length;
+        }
+        return size;
+    }	
+
+	@Override
+	public long getSize() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'getSize'");
 	}
 
-	private int getNodeDiskSize(BTreeNode node) {
-	    int size = Integer.BYTES; // for 'n'
-	    size += 1; // for isLeaf
-	    size += Long.BYTES * node.keys.length; // for keys
-	    if (!node.isLeaf) {
-	        size += Long.BYTES * node.children.length; // for children addresses
-	    }
-	    return size;
-	}	
+	@Override
+	public int getDegree() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'getDegree'");
+	}
 
-  @Override
-  public long getSize() {
-      // TODO Auto-generated method stub
-      throw new UnsupportedOperationException("Unimplemented method 'getSize'");
-  }
+	@Override
+	public int getNumberOfNodes() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'getNumberOfNodes'");
+	}
 
-  @Override
-  public int getDegree() {
-      // TODO Auto-generated method stub
-      throw new UnsupportedOperationException("Unimplemented method 'getDegree'");
-  }
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'getHeight'");
+	}
 
-  @Override
-  public int getNumberOfNodes() {
-      // TODO Auto-generated method stub
-      throw new UnsupportedOperationException("Unimplemented method 'getNumberOfNodes'");
-  }
+	@Override
+	public void delete(long key) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'delete'");
+	}
 
-  @Override
-  public int getHeight() {
-      // TODO Auto-generated method stub
-      throw new UnsupportedOperationException("Unimplemented method 'getHeight'");
-  }
+	@Override
+	public void insert(TreeObject obj) throws IOException {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'insert'");
+	}
 
-  @Override
-  public void delete(long key) {
-      // TODO Auto-generated method stub
-      throw new UnsupportedOperationException("Unimplemented method 'delete'");
-  }
+	@Override
+	public void dumpToFile(PrintWriter out) throws IOException {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'dumpToFile'");
+	}
 
-  @Override
-  public void insert(TreeObject obj) throws IOException {
-      // TODO Auto-generated method stub
-      throw new UnsupportedOperationException("Unimplemented method 'insert'");
-  }
+	@Override
+	public TreeObject search(long key) throws IOException {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'search'");
+	}
 
-  @Override
-  public void dumpToFile(PrintWriter out) throws IOException {
-      // TODO Auto-generated method stub
-      throw new UnsupportedOperationException("Unimplemented method 'dumpToFile'");
-  }
+	// size of file, address of root node, degree, height, numNodes
+	public int getMetaDataSize() {
+		return Long.BYTES * 2 + Integer.BYTES * 3;
+	}
 
-  @Override
-  public TreeObject search(long key) throws IOException {
-      // TODO Auto-generated method stub
-      throw new UnsupportedOperationException("Unimplemented method 'search'");
-  }
+	/**
+	 * write meta data to top of the file
+	 * DO NOT change the write order
+	 */
+	public void writeMetaData() {
+		try {
+			channel.position(0); // set to start of file
+			ByteBuffer buffer = ByteBuffer.allocate(getMetaDataSize());
 
-// size of file, address of root node, degree, height, numNodes
-  public int getMetaDataSize() {
-      return Long.BYTES * 2 + Integer.BYTES * 3;
-  }
+			buffer.putLong(size);
+			buffer.putLong(root.getPointer());
+			buffer.putInt(degree);
+			buffer.putInt(height);
+			buffer.putInt(nodeCount);
 
-  /**
-   * write meta data to top of the file
-   * DO NOT change the write order
-   */
-  public void writeMetaData() {
-      try {
-          channel.position(0); // set to start of file
-          ByteBuffer buffer = ByteBuffer.allocate(getMetaDataSize());
-
-          buffer.putLong(size);
-          buffer.putLong(root.getPointer());
-          buffer.putInt(degree);
-          buffer.putInt(height);
-          buffer.putInt(nodeCount);
-
-          channel.write(buffer);
-      } catch (IOException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-      }
-   }
+			channel.write(buffer);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/main/java/cs321/btree/BTreeNode.java
+++ src/main/java/cs321/btree/BTreeNode.java
@@ -2,6 +2,10 @@ package cs321.btree;
 
 import java.nio.ByteBuffer;
 
+/**
+ * Represents a node in a B-Tree structure. Each node can have multiple keys and children,
+ * depending on the degree of the B-Tree.
+ */
 public class BTreeNode {
     int n;
     long[] keys;
@@ -11,6 +15,12 @@ public class BTreeNode {
     private long pointer; // where we can start reading this node in the file
     static public int NODE_SIZE; // node size in bytes TODO
 
+    /**
+     * Constructs a BTreeNode with a specified degree.
+     * Initializes arrays for keys and children based on the degree.
+     *
+     * @param t the degree of the B-Tree, which dictates the number of maximum children and keys
+     */
     public BTreeNode(int t) {
         keys = new long[2 * t - 1];  // max keys
         children = new long[2 * t];  // max children
@@ -19,14 +29,21 @@ public class BTreeNode {
         diskAddress = -1;  // will be set when writing to disk
     }
     
+    /**
+     * Gets the pointer to this node's location in the file.
+     *
+     * @return the pointer to where this node is stored in the file
+     */
     public long getPointer() {
         return pointer;
     }
 
     /**
-     * TODO read the relevant data from the ByteBuffer and use it to construct a BTreeNode object.
-     * @param buffer
-     * @return
+     * Converts a ByteBuffer into a BTreeNode object. This method should read
+     * the ByteBuffer and extract the necessary information to create a populated BTreeNode.
+     * 
+     * @param buffer the ByteBuffer containing the node's serialized data
+     * @return a new BTreeNode object constructed from the data in the buffer
      */
     public static BTreeNode fromByteBuffer(ByteBuffer buffer) {
         throw new UnsupportedOperationException("Unimplemented method 'fromByteBuffer'");
