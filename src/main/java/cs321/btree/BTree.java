3d9dc566e2fecd42ce8b334593d2a29bf81dfe11
andrewkobus <andrewkobus@u.boisestate.edu>
2024-04-11 19:15:47 -0600

    initialized BTree with interface methods

--- src/main/java/cs321/btree/BTree.java
+++ src/main/java/cs321/btree/BTree.java
@@ -1,6 +1,60 @@
 package cs321.btree;
 
-public class BTree
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class BTree implements BTreeInterface
 {
 
+    public BTree() {
+        // TODO constructor
+    }
+
+    @Override
+    public long getSize() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSize'");
+    }
+
+    @Override
+    public int getDegree() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDegree'");
+    }
+
+    @Override
+    public int getNumberOfNodes() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNumberOfNodes'");
+    }
+
+    @Override
+    public int getHeight() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHeight'");
+    }
+
+    @Override
+    public void delete(long key) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public void insert(TreeObject obj) throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public void dumpToFile(PrintWriter out) throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dumpToFile'");
+    }
+
+    @Override
+    public TreeObject search(long key) throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'search'");
+    }
 }
