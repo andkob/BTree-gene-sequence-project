3758665b209990b86e015afea8d89f5e0438458f
andrewkobus <andrewkobus@u.boisestate.edu>
2024-04-12 15:46:29 -0600

    in progress

---
+++ src/main/java/cs321/btree/BTreeNode.java
@@ -0,0 +1,13 @@
+package cs321.btree;
+
+/**
+ * BTreeNode
+ */
+public class BTreeNode {
+
+    private long pointer; // where we can start reading this node in the file
+
+    public long getPointer() {
+        return pointer;
+    }
+}
