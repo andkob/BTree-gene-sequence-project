367eccf381f6ade660113b1f95b46018f7e6024f
andrewkobus <andrewkobus@u.boisestate.edu>
2024-04-16 14:26:42 -0600

    Updated BTreeNode.java

--- src/main/java/cs321/btree/BTreeNode.java
+++ src/main/java/cs321/btree/BTreeNode.java
@@ -3,24 +3,56 @@ package cs321.btree;
 import java.nio.ByteBuffer;
 
 public class BTreeNode {
-    int n;
-    long[] keys;
-    long[] children;
-    boolean isLeaf;
-    long diskAddress;
-    private long pointer; // where we can start reading this node in the file
-    static public int NODE_SIZE; // node size in bytes TODO
+    // This order will be the order of the node's metadata on the disk. DO NOT change this order.
+    public int numKeys;
+    public boolean isLeaf;
+    private long parentPointer;
+    private int location; // the byte offset in the file
+    public TreeObject[] objects;
+    public long[] children; // child pointers
 
-    public BTreeNode(int t) {
-        keys = new long[2 * t - 1];  // max keys
-        children = new long[2 * t];  // max children
+    /**
+     * Basic constructor for a BTree Node
+     */
+    public BTreeNode(int degree) {
+        objects = new TreeObject[2 * degree - 1];
+        children = new long[2 * degree];
         isLeaf = true;
-        n = 0;
-        diskAddress = -1;  // will be set when writing to disk
+        numKeys = 0;
     }
-    
-    public long getPointer() {
-        return pointer;
+
+    /**
+     * Returns the size of this node in bytes
+     * @return the size of this node in bytes
+     */
+    public int getNodeSize() {
+        // objectsSize + childrenSIze + parentPointerSize + locationSize + isLeafSize + numKeysSize
+        return TreeObject.SIZE * objects.length + Long.BYTES * children.length
+                + Long.BYTES + Integer.BYTES + 1 + Integer.BYTES;
+    }
+
+    /**
+     * Returns the location of this node in terms of Byte offset
+     * @return the Byte offset of this node on the disk
+     */
+    public int getLocation() {
+        return location;
+    }
+
+    /**
+     * Sets the Byte offset in the random access file of this node
+     * @param location the location of this node in the random access file (Byte offset)
+     */
+    public void setLocation(int location) {
+        this.location = location;
+    }
+
+    /**
+     * Sets the parent pointer of this node
+     * @param parentPointer the pointer to this node's parent
+     */
+    public void setParent(long parentPointer) {
+        this.parentPointer = parentPointer;
     }
 
     /**
