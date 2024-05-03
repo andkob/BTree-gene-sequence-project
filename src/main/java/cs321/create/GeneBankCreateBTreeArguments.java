11c202ed02f9d5c36eeaa99ade2e0a6e547e2ad4
Damon <damonwargo@u.boisestate.edu>
2024-05-03 14:53:20 -0600

    CreateBTreeArgs and CreateBTree updated

--- src/main/java/cs321/create/GeneBankCreateBTreeArguments.java
+++ src/main/java/cs321/create/GeneBankCreateBTreeArguments.java
@@ -1,113 +1,151 @@
 package cs321.create;
 
-public class GeneBankCreateBTreeArguments
-{
-    private final boolean useCache;
-    private final int degree;
-    private final String gbkFileName;
-    private final int subsequenceLength;
-    private final int cacheSize;
-    private final int debugLevel;
+import java.io.File;
 
-    public GeneBankCreateBTreeArguments(boolean useCache, int degree, String gbkFileName, int subsequenceLength, int cacheSize, int debugLevel)
-    {
-        this.useCache = useCache;
-        this.degree = degree;
-        this.gbkFileName = gbkFileName;
-        this.subsequenceLength = subsequenceLength;
-        this.cacheSize = cacheSize;
-        this.debugLevel = debugLevel;
-    }
+public class GeneBankCreateBTreeArguments {
+    private int useCache;
+    private int degree;
+    private String gbkFileName;
+    private int subsequenceLength;
+    private int cacheSize;
+    private int debugLevel;
+    private boolean validArgs;
 
-    @Override
-    public boolean equals(Object obj)
-    {
-        //this method was generated using an IDE
-        if (this == obj)
-        {
-            return true;
+    //TODO add comments
+
+    public GeneBankCreateBTreeArguments(String[] args) {
+
+        if (args.length < 4 || args.length > 6) {
+            printUsage();
+            validArgs = false;
+            return;
         }
-        if (obj == null)
-        {
-            return false;
-        }
-        if (getClass() != obj.getClass())
-        {
-            return false;
-        }
-        GeneBankCreateBTreeArguments other = (GeneBankCreateBTreeArguments) obj;
-        if (cacheSize != other.cacheSize)
-        {
-            return false;
-        }
-        if (debugLevel != other.debugLevel)
-        {
-            return false;
-        }
-        if (degree != other.degree)
-        {
-            return false;
-        }
-        if (gbkFileName == null)
-        {
-            if (other.gbkFileName != null)
-            {
-                return false;
+
+        useCache = -1;
+        degree = -1;
+        gbkFileName = "";
+        subsequenceLength = -1;
+        cacheSize = -1;
+        debugLevel = 0;
+        validArgs = true;
+
+        for(String arg : args) {
+
+            try {
+                if(arg.length() >= 8 && arg.substring(0, 8).equals("--cache=")) {
+                    useCache = Integer.parseInt(arg.substring(8));
+                    System.out.println("useCache: " + useCache);
+                } else if (arg.length() >= 9 && arg.substring(0, 9).equals("--degree=")) {
+                    degree = Integer.parseInt(arg.substring(9));
+                    System.out.println("degree: " + degree);
+                } else if (arg.length() >= 10 && arg.substring(0, 10).equals("--gbkfile=")) {
+                    gbkFileName = arg.substring(10);
+                    System.out.println("gbkFileName: " + gbkFileName);
+                } else if (arg.length() >= 9 && arg.substring(0,9).equals("--length=")) {
+                    subsequenceLength = Integer.parseInt(arg.substring(9));
+                    System.out.println("length: " + subsequenceLength);
+                } else if (arg.length() >= 8 && arg.substring(0, 8).equals("--debug=")) {
+                    debugLevel = Integer.parseInt(arg.substring(8));
+                    System.out.println("debugLevel: " + debugLevel);
+                } else if (arg.length() >= 12 && arg.substring(0, 12).equals("--cachesize=")) {
+                    cacheSize = Integer.parseInt(arg.substring(12));
+                    System.out.println("cacheSize: " + cacheSize);
+                } else {
+                    System.out.println("Error: invalid arg " + arg);
+                    validArgs = false;
+                    printUsage();
+                    return;
+                }
+            } catch (Exception e) {
+                System.out.println(e.toString());
+                validArgs = false;
+                printUsage();
+                return;
             }
         }
-        else
-        {
-            if (!gbkFileName.equals(other.gbkFileName))
-            {
-                return false;
+
+        if (useCache != 0 && useCache != 1) {
+            validArgs = false;
+            System.out.println("Error: invalid arg --cache=" + useCache);
+            printUsage();
+            return;
+        }
+
+        if (degree < 0) {
+            validArgs = false;
+            System.out.println("Error: invalid arg --degree=" + degree);
+            printUsage();
+            return;
+        }
+
+        try {
+            File f = new File(gbkFileName);
+            if (!f.exists()) {
+                validArgs = false;
+                System.out.println("Error: invalid arg --gbkfile=" + gbkFileName);
+                printUsage();
+                return;
             }
+        } catch (Exception e) {
+            validArgs = false;
+            System.out.println(e.toString());
+            printUsage();
+            return;
         }
-        if (subsequenceLength != other.subsequenceLength)
-        {
-            return false;
+
+        if(subsequenceLength < 1 || subsequenceLength > 31) {
+            validArgs = false;
+            System.out.println("Error: invalid arg --length=" + subsequenceLength);
+            printUsage();
+            return;
         }
-        if (useCache != other.useCache)
-        {
-            return false;
+
+        if(useCache == 1 && (cacheSize < 100 || cacheSize > 10000)) {
+            validArgs = false;
+            System.out.println("Error: invalid arg --cachesize=" + cacheSize);
+            printUsage();
         }
-        return true;
+
+        if(debugLevel != 0 && debugLevel != 1) {
+            validArgs = false;
+            System.out.println("Error: invalid arg --debug=" + debugLevel);
+        }
+
     }
 
-    @Override
-    public String toString()
-    {
-        //this method was generated using an IDE
-        return "GeneBankCreateBTreeArguments{" +
-                "useCache=" + useCache +
-                ", degree=" + degree +
-                ", gbkFileName='" + gbkFileName + '\'' +
-                ", subsequenceLength=" + subsequenceLength +
-                ", cacheSize=" + cacheSize +
-                ", debugLevel=" + debugLevel +
-                '}';
+    public void printUsage() {
+        System.out.println("Usage: java -jar build/libs/GeneBankCreateBTree.jar --cache=<0|1>  --degree=<btree-degree> --gbkfile=<gbk-file> --length=<sequence-length> [--cachesize=<n>] [--debug=0|1]");
+    }
+
+    public boolean validate() {
+        return validArgs;
     }
 
 	public String getGbkFileName() {
-		return this.gbkFileName;
+		return gbkFileName;
 	}
 
 	public int getDegree() {
-		return this.degree;
+		return degree;
 	}
 
 	public int getSubsequenceLength() {
-		return this.subsequenceLength;
+		return subsequenceLength;
 	}
 
 	public boolean getUseCache() {
-		return this.useCache;
+		if (useCache == 0) {
+            return false;
+        } else {
+            return true;
+        }
 	}
 
 	public int getCacheSize() {
-		return this.cacheSize;
+		return cacheSize;
 	}
 
 	public int getDebugLevel() {
-		return this.debugLevel;
+		return debugLevel;
 	}
 }
